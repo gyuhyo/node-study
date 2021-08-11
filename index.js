a442c10a33c0d556d79f2a9e74e80666aba73591
injin <picablue6252@hanmail.net>
2021-08-11 15:41:26 +0900

    secret information policy

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://qkrrbgy:gkdlfn22@study.1yz5z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('HelloWorld'))
+
+/*********************************
+ * Router
+ *********************************/
+app.get('/', (req, res) => res.send('HelloWorldasdasd'))
+
+app.post('/register', (req, res) => {
+    //회원가입할 때 필요한 정보를 Client에서 가져오면 데이터베이스에 저장
+    const user = new User(req.body);
+    user.save((err, userinfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
